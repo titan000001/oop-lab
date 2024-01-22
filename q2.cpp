9447c02e5d70f2a08e0e364a15fa2f5bd6da1996
Tawhid <tawhid.10a@gmail.com>
2024-01-22 12:00:38 +0600

    added topic_3 in the folder

---
+++ q2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    bool isStudent = true;
+    long long studentID = 41230201224;
+
+    cout << "Is he a student? : " << isStudent << '\n' << "His student ID is : " << studentID;
+    
+    return 0;
+}
---
+++ topic_3/q1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int height = 169;
+    int weight = 74;
+    string eye_colour = "black";
+
+        cout << "My height is " << height << "cm, My weight is " << weight << " kg, and my eye colour is " << eye_colour;
+    
+    return 0;
+}
